1eb4269b8b81603830ca18b6653f50ac165d05a7
Cian O'Siorain <Ciano930@live.de>
2018-02-21 12:46:55 +0000

    Added some basic routing for testing

--- BattleReportAppVer1/routes/web.php
+++ BattleReportAppVer1/routes/web.php
@@ -1,16 +1,42 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+**--------------------------------------------------------------------------
+** Web Routes
+**--------------------------------------------------------------------------
+** Basic Routes for a RESTful service:
+**
+** Route::get($uri, $callback);
+** Route::post($uri, $callback);
+** Route::put($uri, $callback);
+** Route::delete($uri, $callback);
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('reports', function(){
+  return response(['Report 1', 'Report 2', 'Report 3'], 200);
+});
+
+Route::get('reports/{report}', function ($reportId) {
+    return response()->json(['reportId' => "{$reportId}"], 200);
+});
+
+
+Route::post('reports', function() {
+    return  response()->json([
+            'message' => 'Create success'
+        ], 201);
+});
+
+Route::put('reports/{report}', function() {
+    return  response()->json([
+            'message' => 'Update success'
+        ], 200);
+});
+
+Route::delete('reports/{report}',function() {
+    return  response()->json(null, 204);
 });
